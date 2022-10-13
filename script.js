ee96274efa81de1b1997221907299a801b7940ae
Sac6567 <sac6567@gmail.com>
2022-10-13 17:50:52 +0530

    base

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  // canvas setup
+  const canvas = this.document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d"); //use to draw and animate 2d/webgl
+  canvas.width = 1500;
+  canvas.height = 500;
+});
